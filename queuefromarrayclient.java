26256c2d2e131b2cb85978a47aab3d19e545e4ac
Akagami_Shanks <41580494+bhardwaj-aman@users.noreply.github.com>
2019-06-06 12:48:27 +0530

    Delete queuefromarrayclient.java

--- queuefromarrayclient.java
+++
@@ -1,9 +0,0 @@
-package stackandqueue;
-public class queuefromarrayclient {
-    public static void main(String[] args) throws Exception{
-        queuefromarray q = new queuefromarray(5);
-        q.enqueue(69);
-        q.enqueue(70);
-        q.print();
-    }
-}
